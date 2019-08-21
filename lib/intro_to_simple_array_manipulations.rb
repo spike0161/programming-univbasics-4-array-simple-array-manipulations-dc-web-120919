98088ea4adf2bfd1fb7117c4316e7c970cb11590
Dean Hildebrand <dean.hildebrand88@gmail.com>
2019-08-21 22:51:17 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -2,21 +2,26 @@ def using_push(array,string)
   array.push(string)
 end
 
+
 def using_unshift(array,string)
   array.unshift(string)
 end
+
 
 def using_pop(array)
   array.pop
 end
 
+
 def pop_with_args(array)
   array.pop(2)
 end
 
+
 def using_shift(array)
   array.shift
 end
+
 
 def shift_with_args(array)
   array.shift(2)
@@ -27,14 +32,30 @@ def using_concat(array1,array2)
   array1.concat(array2)
 end
 
+
 def using_insert(array,new_element)
   array.insert(4, new_element)
 end
+
 
 def using_uniq(array)
   array.uniq
 end
 
+
+def using_flatten(array,string)
+  
+end
+
+
+def using_delete()
+  
+end
+
+
+def using_delete_at()
+  
+end
   
   
   
